62f83f738c42577f4241adff2804f9e0e08bccc3
milan-c-00 <cvijovic.m21@gmail.com>
2025-03-19 20:31:45 +0100

    Started making auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,9 @@ use MongoDB\Laravel\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
 
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'role'];
 
     protected $hidden = ['password', 'remember_token'];
-
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
